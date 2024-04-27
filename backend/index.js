db7a7ca84dcd93db3d188e8e57184d7ea24b12ab
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2024-04-27 19:07:56 +0300

    new

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const { connectToMongoDB } = require("./database");
-// const path = require("path")
+const path = require("path")
 
 const app = express();
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "build")))
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "build/index.html"))
-// })
+app.use(express.static(path.join(__dirname, "build")))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build/index.html"))
+})
 
 const router = require("./routes");
 app.use("/api", router);
